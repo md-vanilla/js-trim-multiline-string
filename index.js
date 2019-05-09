b1b383d20804b06c5240a423d6e97fb0afe131a0
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-09 14:43:19 -0400

    add support for tagged literals

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function mTrim ( str ) {
+  if ( Array.isArray(str) ) { str = str[0] }
   if ( ! str ) { str = this; }
-  return str.split( '\n' ).map(x => x.trim()).join('\n')
+  if ( str ) {
+    return str.split( '\n' ).map(x => x.trim()).join('\n')
+  }
 }
 String.prototype.mtrim = mTrim;
 module.exports = String;
 module.exports = { mtrim: mTrim };
+
 
