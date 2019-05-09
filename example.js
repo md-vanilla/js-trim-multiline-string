1b48fe1edfc6aa26c9daecb2e3bf460c32439904
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-09 14:43:52 -0400

    implementation examples for the application

--- example.js
+++ example.js
@@ -12,6 +12,25 @@ console.log(
           example: ${example}
 
           trimmed: ${example.mtrim()}
+
+          alt-trimmed: ${mtrim(example)}
+
   `
 )
 
+console.log(
+  'example0: '+mtrim(example),
+  "\n\n"+'example1: '+"\n\n"+mtrim(example),
+  "\n\n"+'example2: '+"\n\n"+example.mtrim()
+)
+
+console.log(
+  mtrim`     example3
+  this
+     is
+       a
+   multiline
+     string
+  `
+)
+// not supported: console.log( mtrim`{example}` )
