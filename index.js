de920ad91d94ab4bc47e5471665e4713dc508802
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-15 17:21:14 -0400

    trim leading white spaces but retain formatting if that was intentional.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const numSpaces = (lines) =>
+  lines.reduce((acc, cur) => {
+    const  index = cur.search(/[^\s]/)
+    return ( index > 0 && ( acc == 0  || index < acc ) ) ? index : acc
+  }, 0)
+
+
 function mTrim ( str ) { // parameter is for the exporting mtrim function.
+  // console.log(str)
   if ( Array.isArray(str) ) { str = str[0] } // for tagged template implementation.
   if ( ! str ) { str = this; } // for prototype implementation.
   if ( str ) {
-    return str.split( '\n' ).map(x => x.trim()).join('\n')
+
+    const lines = str.split( '\n' )
+    const delSpacesNum = numSpaces(lines)
+
+    return lines.map(x => x.substring(delSpacesNum)).join('\n')
   }
 }
+
 String.prototype.mtrim = mTrim;
 module.exports = String;
 module.exports = { mtrim: mTrim };
